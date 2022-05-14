89a032555d43597362f869f2d585c731bb05ec1e
mansur <mansuriosdevm1@gmail.com>
2022-05-14 10:25:46 +0500

    yangi kun

---
+++ lib/pages/new_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NewPage extends StatefulWidget {
+  static final String id ="new_ page";
+  const NewPage({Key? key}) : super(key: key);
+
+  @override
+  State<NewPage> createState() => _NewPageState();
+}
+
+class _NewPageState extends State<NewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
